daa7f3f5020bf3e0c1528c820135d815b6d76842
Pocinho <72917055+H-Pocinho@users.noreply.github.com>
2021-03-17 15:14:05 +0000

    Update lab1.s

--- Lab_1/lab1.s
+++ Lab_1/lab1.s
@@ -8,7 +8,7 @@ li a5,10		#numero de membros que se deseja da sucessão
 la a0,u1    #Load da adress do primeiro membro da sucessão (x10=a0 vai ter o valor do endereço de u1)
 
 while:
-lw a1,0(a0)		  #Load dos valores da memoria
+lw a1,0(a0)     #Load dos valores da memoria
 lw a2,4(a0)
 add a3,a2,a1	  #soma dos membros
 sw a3,8(a0)       #guarda o valor da soma de un=u(n-2)+u(n-1)
