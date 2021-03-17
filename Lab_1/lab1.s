70be9e7eb6513852fa25a41eb6d3136d4f4c0168
Pocinho <72917055+H-Pocinho@users.noreply.github.com>
2021-03-17 15:13:38 +0000

    Update lab1.s

--- Lab_1/lab1.s
+++ Lab_1/lab1.s
@@ -11,7 +11,7 @@ while:
 lw a1,0(a0)		  #Load dos valores da memoria
 lw a2,4(a0)
 add a3,a2,a1	  #soma dos membros
-sw a3,8(a0)		  #guarda o valor da soma de un=u(n-2)+u(n-1)
+sw a3,8(a0)    #guarda o valor da soma de un=u(n-2)+u(n-1)
 addi a4,a4,1	  #contagem do numero de ciclos
 addi a0,a0,4	  #salta para a posição de memoria seguinte
 blt a4,a5,while   #repete o ciclo até atingir o numero definido na linha 7
