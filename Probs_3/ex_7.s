8e3a16634d14f8b201593e519a0e9afd851269c4
Pocinho <72917055+H-Pocinho@users.noreply.github.com>
2021-04-07 12:07:12 +0100

    Update ex_7.s

--- Probs_3/ex_7.s
+++ Probs_3/ex_7.s
@@ -1,27 +1,30 @@
 #Criado por Henrique Alves Pocinho IST199952
+.data
+n: .word 6
+adr: .word 0x50
 
 .text
-li x10,6
-li x1,0x48
+lw x10,n
+lw x1,adr
 
 fact:
 addi sp,sp,-8
 sw	 x1,4(sp) 
 sw 	 x10,0(sp) 
 addi x5, x10,-1 
-bge  x5,x0,L1 
+bge  x5,x0,Nzer0 
 addi x10,x0,1 
 addi sp,sp,8
 jalr x0,x1,0
 
-L1: 
+Nzer0: 
 addi x10,x10,-1 
 jal  x1,fact 
-addi x6,x10,0 
+addi x11,x10,0 
 lw 	 x10,0(sp) 
 lw 	 x1,4(sp) 
 addi sp,sp,8
-mul  x10, x10,x6 
+mul  x10, x10,x11 
 jalr x0,x1,0
 
 li x17,10
