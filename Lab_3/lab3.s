6502d0c9aef428a7be6b1448b2044ecf5a241cc4
Pocinho <hapocinho@gmail.com>
2021-04-25 00:22:04 +0100

    add lab 3

---
+++ Lab_3/lab3.s
@@ -0,0 +1,21 @@
+# 1)
+# Rotinas:
+#   -Pow: 
+#       Efeitos: Calcula x^y
+#       Metodo de passagem: Registos
+#       Parametros utilizados: a0 - x e a1 - y
+#       (Contar as rotinas que fazem parte da rotina?)
+#
+#   -Mult
+#       Efeitos: Calcula a*b
+#       Metodo de passagem: Pilha
+#       Parametros utilizados: 0(sp) - a e 4(sp) - b
+#
+#
+# 2) A rotina Pow recebe e devolve os parametros atraves de registos (entry:a0 e a1 | return:a0)
+#
+# 3) Antes(PC): 12 Depois(ra): 20
+#
+# 4) 
+#
+# 5)
